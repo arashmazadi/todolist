5bdf6278d5e63afcb4082f84c149b693222c0d64
arashmazadi <mazadi.arash@gmail.com>
2024-04-06 17:43:42 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+TodoList = [];
+
+app.get("/", (req, res) => {
+  res.send(TodoList);
+});
+
+app.post("/post", (req, res) => {
+  inputtodo = {
+    todoID: TodoList.length + 1,
+    todoTitle: req.body.title,
+  };
+  TodoList.push(inputtodo);
+  res.json(inputtodo);
+});
+
+// app.put();
+
+// app.delete();
+
+app.listen(port, () => {
+  console.log(`app on listening on port ${port}`);
+});
---
+++ todos.js
@@ -0,0 +1,12 @@
+const Todos = [
+  {
+    id: 1,
+    todo: "first",
+    id: 2,
+    todo: "second",
+    id: 3,
+    todo: "third",
+  },
+];
+
+module.exports = Todos;
